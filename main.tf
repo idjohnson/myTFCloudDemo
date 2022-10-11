1c3c3ed18e6dc4aa53ed3600eeba17bc709f835c
Isaac Johnson <isaac.johnson@gmail.com>
2022-10-11 07:22:40 -0500

    a main that fails OPA

--- main.tf
+++ main.tf
@@ -10,11 +10,11 @@ resource "aws_instance" "web" {
 resource "aws_autoscaling_group" "my_asg" {
   availability_zones        = ["us-west-1a"]
   name                      = "my_asg"
-  max_size                  = 5
-  min_size                  = 1
+  max_size                  = 70
+  min_size                  = 50
   health_check_grace_period = 300
   health_check_type         = "ELB"
-  desired_capacity          = 4
+  desired_capacity          = 50
   force_delete              = true
   launch_configuration      = "my_web_config"
 }
@@ -24,3 +24,29 @@ resource "aws_launch_configuration" "my_web_config" {
     image_id = "ami-02ef5566632e904f8"
     instance_type = "t2.micro"
 }
+
+resource "aws_autoscaling_group" "my_asg2" {
+  availability_zones        = ["us-west-1b"]
+  name                      = "my_asg"
+  max_size                  = 70
+  min_size                  = 50
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 50
+  force_delete              = true
+  launch_configuration      = "my_web_config"
+}
+
+
+resource "aws_autoscaling_group" "my_asg3" {
+  availability_zones        = ["us-west-1c"]
+  name                      = "my_asg"
+  max_size                  = 70
+  min_size                  = 50
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 50
+  force_delete              = true
+  launch_configuration      = "my_web_config"
+}
+
