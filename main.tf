d2153caec9c460eb67abc4297809489203d35bbd
Isaac Johnson <isaac.johnson@gmail.com>
2022-10-12 07:07:40 -0500

    comment out 2 ASGs

--- main.tf
+++ main.tf
@@ -25,28 +25,28 @@ resource "aws_launch_configuration" "my_web_config" {
     instance_type = "t2.micro"
 }
 
-resource "aws_autoscaling_group" "my_asg2" {
-  availability_zones        = ["us-west-1b"]
-  name                      = "my_asg"
-  max_size                  = 70
-  min_size                  = 50
-  health_check_grace_period = 300
-  health_check_type         = "ELB"
-  desired_capacity          = 50
-  force_delete              = true
-  launch_configuration      = "my_web_config"
-}
-
-
-resource "aws_autoscaling_group" "my_asg3" {
-  availability_zones        = ["us-west-1c"]
-  name                      = "my_asg"
-  max_size                  = 70
-  min_size                  = 50
-  health_check_grace_period = 300
-  health_check_type         = "ELB"
-  desired_capacity          = 50
-  force_delete              = true
-  launch_configuration      = "my_web_config"
-}
-
+# resource "aws_autoscaling_group" "my_asg2" {
+#   availability_zones        = ["us-west-1b"]
+#   name                      = "my_asg"
+#   max_size                  = 70
+#   min_size                  = 50
+#   health_check_grace_period = 300
+#   health_check_type         = "ELB"
+#   desired_capacity          = 50
+#   force_delete              = true
+#   launch_configuration      = "my_web_config"
+# }
+# 
+# 
+# resource "aws_autoscaling_group" "my_asg3" {
+#   availability_zones        = ["us-west-1c"]
+#   name                      = "my_asg"
+#   max_size                  = 70
+#   min_size                  = 50
+#   health_check_grace_period = 300
+#   health_check_type         = "ELB"
+#   desired_capacity          = 50
+#   force_delete              = true
+#   launch_configuration      = "my_web_config"
+# }
+# 
